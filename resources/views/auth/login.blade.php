27826a852d4ee24313bebc5689cae8b4540d699e
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-04-02 02:22:29 +0530

    fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
     <div class="card-body">
       <!-- <p class="login-box-msg">Sign in to start your session</p> -->
 
-      <form action="{{ route('login') }}" method="post">
+      <form action="{{ asset('login') }}" method="post">
       @csrf
         <div class="input-group mb-3">
           <input type="email" class="form-control" name="email" placeholder="{{ __('Email Address') }}">
