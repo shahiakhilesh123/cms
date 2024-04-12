27e5b868a1ca62653da965acde1fb62c289c216c
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-04-12 10:54:34 +0530

    fix

--- resources/views/admin/blogList.blade.php
+++ resources/views/admin/blogList.blade.php
@@ -57,8 +57,8 @@
                       @foreach($blogs as $blog)
                       <tr>
                         <td>{{ $blog->id }}</td>
-                        <td style="width:30%;">{{ $blog->name }}</td>
-                        <td style="width:30%;">{{ $blog->link }}</td>                      
+                        <td style="width:200px;">{{ $blog->name }}</td>
+                        <td style="width:200px;">{{ $blog->link }}</td>                      
                         <td>
                           <!-- <a href="{{ asset('blogs') }}/{{$blog->id}}/{{ str_replace(" ","-",$blog->name) }}" target="_blank"><i class="fas fa-copy"></i></a> -->
                           <a href="{{ asset('posts/edit') }}/{{$blog->id}}"><i class="fas fa-edit"></i></a>
